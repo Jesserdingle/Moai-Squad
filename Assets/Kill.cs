ff4c5005f7efe2cc440f3472864d2fda3d540dc8
Jesserdingle <anteklipinski07@gmail.com>
2022-12-03 05:28:08 +0100

    animacja nowa biegu

---
+++ Assets/Kill.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class kill : MonoBehaviour
+{
+    public GameObject player;
+    public Transform respawnPoint;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
