2ba44c385d62e37935c4df245aa891fb0254f48e
Jesserdingle <anteklipinski07@gmail.com>
2022-12-03 05:51:24 +0100

    respawn + animacja idle

--- Assets/Kill.cs
+++ Assets/Kill.cs
@@ -2,10 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class kill : MonoBehaviour
+public class Kill : MonoBehaviour
 {
     public GameObject player;
     public Transform respawnPoint;
+
+    // Start is called before the first frame update
     void Start()
     {
         
@@ -15,5 +17,12 @@ public class kill : MonoBehaviour
     void Update()
     {
         
+    }
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            player.transform.position = respawnPoint.position;
+        }
     }
 }
